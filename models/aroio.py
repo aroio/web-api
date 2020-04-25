7cd43382231f4a1e7cd301da6cc94cb9e43ed765
Melf Stöcken <melf.stoecken@googlemail.com>
2020-04-25 13:00:50 +0200

    updated database schema

--- models/aroio.py
+++ models/aroio.py
@@ -4,12 +4,17 @@ import json
 
 class NetworkConfig(BaseModel):
         hostname: str
-        dhcp: bool
         wifi: bool
-        ipaddr: Optional[str] = None
-        netmask: Optional[str] = None
-        dnsserv: Optional[str] = None
-        gateway: Optional[str] = None
+        lan_dhcp: Optional[bool] = True
+        lan_ipaddr: Optional[str] = None
+        lan_netmask: Optional[str] = None
+        lan_dnsserv: Optional[str] = None
+        lan_gateway: Optional[str] = None
+        wlan_dhcp: Optional[bool] = True
+        wlan_ipaddr: Optional[str] = None
+        wlan_netmask: Optional[str] = None
+        wlan_dnsserv: Optional[str] = None
+        wlan_gateway: Optional[str] = None
         wlanssid: Optional[str] = None
         wlanpwd: Optional[str] = None
 
@@ -17,10 +22,13 @@ class NetworkConfig(BaseModel):
 class SystemConfig(BaseModel):
     updateserver: str
     betaserver: str
-    usebeta: str
+    usebeta: bool
     platform: str
     userpasswd: str
-
+    known_version: str
+    btkey: str
+    advanced: bool
+    display_rotate: bool
 
 class StreamingConfig(BaseModel):
     servername: Optional[str] = None
@@ -33,12 +41,58 @@ class StreamingConfig(BaseModel):
 class AudioConfig(BaseModel):
     audioplayer: str
     rate: str
+    sprate: str
     channels: str
-    mscoding: str
+    mscoding: bool
     volume: str
-    jackbuffer: str
     soundcard: str
-
+    resampling: str
+    volume_start: str
+    audio_output: str
+    measurement_output: str
+    debug: str
+    jackbuffer: str
+    jackperiod: str
+    raw_player: str
+    raw_playerms: str
+    squeeze_maxfrequency: str
+    squeeze_intbuffer: str
+    squeeze_outbuffer: str
+    sp_outbuffer: str
+    sp_period: str
+    bf_partitions: str
+    dmix_squeezelite: Optional[bool] = False
+    dmix_gmediarender: Optional[bool] = False
+    dmix_shairportsync: Optional[bool] = False
+    dmix_bluealsaaplay: Optional[bool] = False
+    dmixms_squeezelite: Optional[bool] = False
+    dmixms_gmediarender: Optional[bool] = False
+    dmixms_shairportsync: Optional[bool] = False
+    dmixms_bluealsaaplay: Optional[bool] = False
+    jack_squeezelite: Optional[bool] = False
+    jack_gmediarender: Optional[bool] = False
+    jack_shairportsync: Optional[bool] = False
+    jack_bluealsaaplay: Optional[bool] = False
+    jack_netjack: Optional[bool] = False
+    jack_input: Optional[bool] = False
+    jackms_squeezelite: Optional[bool] = False
+    jackms_gmediarender: Optional[bool] = False
+    jackms_shairportsync: Optional[bool] = False
+    jackms_bluealsaaplay: Optional[bool] = False
+    jackms_netjack: Optional[bool] = False
+    jackms_input: Optional[bool] = False
+    jackbf_squeezelite: Optional[bool] = False
+    jackbf_gmediarender: Optional[bool] = False
+    jackbf_shairportsync: Optional[bool] = False
+    jackbf_bluealsaaplay: Optional[bool] = False
+    jackbf_netjack: Optional[bool] = False
+    jackbf_input: Optional[bool] = False
+    jackbfms_squeezelite: Optional[bool] = False
+    jackbfms_gmediarender: Optional[bool] = False
+    jackbfms_shairportsync: Optional[bool] = False
+    jackbfms_bluealsaaplay: Optional[bool] = False
+    jackbfms_netjack: Optional[bool] = False
+    jackbfms_input: Optional[bool] = False
 
 class ConvolverConfig(BaseModel):
     debug: str
