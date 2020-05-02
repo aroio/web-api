a7e6e8ae2519be751c678043a67c0c4d16be0421
Melf Stöcken <melf.stoecken@googlemail.com>
2020-05-02 14:08:39 +0200

    adjustments in aroio audio config model, added translations

--- models/aroio.py
+++ models/aroio.py
@@ -48,8 +48,6 @@ class StreamingConfig(BaseModel):
 class PlayerConfig(BaseModel):
     mscoding: bool = False
     measurement_output: str = "vol-plug-ms"
-    rate: int = 176400
-    channels: int = 2
     squeezelite: bool = False
     gmediarender: bool = False
     shairportsync: bool = False
@@ -60,27 +58,32 @@ class PlayerConfig(BaseModel):
 
 class OutputConfig(BaseModel):
     audio_output: str = "vol-plug"
+    rate: int = 44100
     direct_config: PlayerConfig = PlayerConfig()
     bus_config: PlayerConfig = PlayerConfig()
     convolver_config: PlayerConfig = PlayerConfig()
 
 
 class AudioConfig(BaseModel):
+    audioplayer: str = None
+    channels: int = 2
     soundcard: str = "AroioDAC"
     resampling: str = "speexrate_medium"
     volume_start: int = -15
     debug: bool = False
-    jackbuffer: int = 8192
+    jackbuffer: int = 4096
     jackperiod: int = 3
     raw_player: str = "shairportsync"
     raw_playerms: str = "squeezelite"
-    shairplayrate: int = 44100
     squeeze_maxfrequency: int = 192000
-    squeeze_intbuffer: int = 4096
-    squeeze_outbuffer: int = 4096
+    squeeze_alsabuffer: int = 16384
+    squeeze_alsaperiod: int = 4
+    squeeze_intbuffer: int = 16384
+    squeeze_outbuffer: int = 8192
     sp_outbuffer: int = 32768
     sp_period: int = 2
-    brutefirf_partitions: int = 2
+    sp_samplerate: int = 44100
+    bf_partitions: int = 2
     output_configuration: OutputConfig = OutputConfig()
 
 
